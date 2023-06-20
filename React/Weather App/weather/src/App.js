4ce73473400d7240451b35ff20d8c89010e4f36b
Berk Tenkoğlu <berk.tenkoglu@gmail.com>
2023-06-21 01:33:48 +0300

    updated weather app

--- React/Weather App/weather/src/App.js
+++ React/Weather App/weather/src/App.js
@@ -1,13 +1,16 @@
 import './App.css';
 import Header from './components/Header';
 import Weather from "./components/weather";
+import { CityContextProvider } from './contexts/CityContext';
 
 function App() {
   return (
     <div className="App">
       <Header/>
       <hr/>
+      <CityContextProvider>
       <Weather/>
+      </CityContextProvider>
     </div>
   );
 }
--- React/Weather App/weather/src/components/weather.js
+++ React/Weather App/weather/src/components/weather.js
@@ -1,34 +1,50 @@
-import React, { useEffect, useState } from 'react'
+import React, {  useEffect, useState,useContext } from 'react'
 import axios from "axios";
+import CityContext from '../contexts/CityContext';
+
 
 function Weather() {
-    const [weatherData, setWeatherData] = useState();
-    const [selectedCity, setSelectedCity] = useState('istanbul');
-    useEffect(() => {
-        axios( `https://api.openweathermap.org/data/2.5/weather?q=${selectedCity}&units=metric&appid=f5fea04b50574fd876eb0cb8f961c4bc`)
-        .then((res)=> setWeatherData(res.data.main.temp))
-    },[selectedCity]);
+    const [minTemperature, setMinTemperature] = useState();
+    const [maxTemperature, setMaxTemperature] = useState();
+    const [description, setDescription] = useState();
+    const [icon, setIcon] = useState();
 
-    const handleCityChange = (event) => {
-        setSelectedCity(event.target.value);
-      };
-    return (
-        <div>
-            <h1>Weathers</h1>
-                <select value={selectedCity} onChange={handleCityChange}>
-                    <option value={"istanbul"}>İstanbul</option>
-                    <option value={"Ankara"}>Ankara</option>
-                    <option value={"İzmir"}>İzmir</option>
-                </select>
-            {/* {weatherData.map((weather) => (
-                <div key={weather}>
-                    {weather}
-                </div>
-            ))} */}
-            <h2>{weatherData}</h2>
+  const [selectedCity, setSelectedCity] = useState('Adana');
+  const cities = useContext(CityContext);
 
-        </div>
-    )
+  useEffect(() => {
+    axios(`https://api.openweathermap.org/data/2.5/weather?q=${selectedCity}&units=metric&appid=f5fea04b50574fd876eb0cb8f961c4bc`
+    ).then((res) => {
+      setMinTemperature(res.data.main.temp_min);
+      setMaxTemperature(res.data.main.temp_max);
+      setDescription(res.data.weather[0].description);
+      setIcon(res.data.weather[0].icon);
+    })
+    .catch((error) => {
+      console.error('Error fetching weather data:', error);
+    });
+}, [selectedCity]);
+
+  const handleCityChange = (event) => {
+    setSelectedCity(event.target.value);
+  };
+
+  return (
+    <div>
+      <h1>Weathers</h1>
+      <select value={selectedCity} onChange={handleCityChange}>
+        {cities.map((city) => (
+          <option key={city} value={city}>
+            {city}
+          </option>
+        ))}
+      </select>
+      <h2>Min Temperature: {minTemperature}</h2>
+      <h2>Max Temperature: {maxTemperature}</h2>
+      <h2>Description: {description}</h2>
+      {icon && <img src={`http://openweathermap.org/img/w/${icon}.png`} alt="Weather Icon" />}
+    </div>
+  );
 }
 
-export default Weather
+export default Weather;
