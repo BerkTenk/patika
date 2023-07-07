dd018d3879bdb5601337f0d73bb34fe6f6db0613
Berk Tenkoğlu <berk.tenkoglu@gmail.com>
2023-07-07 13:15:46 +0300

    updated

--- React/emoji-search-master-test/src/App.test.js
+++ React/emoji-search-master-test/src/App.test.js
@@ -1,10 +1,10 @@
 import React from "react";
-import { render, screen } from '@testing-library/react';
-import App from "./App";
+import { render, screen,fireEvent } from '@testing-library/react';
 import Header from "./Header";
 import filterEmoji from "./filterEmoji";
-
-
+import App from "./App";
+import userEvent from '@testing-library/user-event';
+import EmojiList from "./EmojiList";
 test('Header component should be rendered successfully', () => {
   render(<Header />);
   const headerElement = screen.getByText('Emoji Search');
@@ -12,18 +12,6 @@ test('Header component should be rendered successfully', () => {
 });
 
 test("Emojilist should be rendered successfully", () => {
-  const emojis = [
-      {
-        "title": "100",
-        "symbol": "💯",
-        "keywords": "hundred points symbol symbol wow wow win win perfect perfect parties parties"
-      },
-      {
-        "title": "1234",
-        "symbol": "🔢",
-        "keywords": "input symbol for numbers symbol"
-      }
-    ];
   const searchText = 'symbol';
   const maxResults = 2;
   const expectedResults = [
@@ -44,3 +32,27 @@ test("Emojilist should be rendered successfully", () => {
   expect(filteredEmojis[0]).toEqual(expectedResults[0]);
 });
 
+test('Filtered emoji list should be rendered successfully', () => {
+  
+  const searchText = 'symbol';
+  const maxResults = 2;
+
+  render(<App/>);
+  
+  const inputElement = screen.getByPlaceholderText('Search emoji...');
+  userEvent.type(inputElement, searchText );
+
+  const filteredEmojis = filterEmoji(searchText, maxResults);
+  
+  filteredEmojis.forEach(emoji => {
+    const emojiElement = screen.getByText(emoji.title);
+    expect(emojiElement).toBeInTheDocument;
+  });
+});
+
+test('Emojis should be coppied successfully', () => { 
+  render(<App/>);
+  const copyEmoji = screen.getByText("Grinning");
+  fireEvent.click(copyEmoji);
+  expect(copyEmoji.parentElement.getAttribute("data-clipboard-text")).toMatch('😀');
+ })
